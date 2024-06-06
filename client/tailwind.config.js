087796d55545c2789e867b1a1a2d8b45073024d4
Alan Meachum <alanmmeachum@outlook.com>
2024-06-05 22:55:48 -0400

    Configured Tailwind and setup the dashboard I will use

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+    './src/**/*.{js,jsx,ts,tsx}',
+],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
